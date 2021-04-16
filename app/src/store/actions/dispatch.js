7c062feeb021b0f62db9d7dc717c6eb3a644f1d7
George Treviranus <3946669+geotrev@users.noreply.github.com>
2021-04-16 10:45:48 -0700

    Update dispatch.js

--- app/src/store/actions/dispatch.js
+++ app/src/store/actions/dispatch.js
@@ -1,10 +1,7 @@
-export const dispatch = (el, type, detail = {}) => {
+export const dispatch = (type, detail = {}) => {
   const event = new CustomEvent(type, {
-    bubbles: true,
     composed: true,
-    currentTarget: el,
-    target: el,
     detail,
   })
-  el.dispatchEvent(event)
+  document.dispatchEvent(event)
 }
