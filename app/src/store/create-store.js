0422bc6ee619c409d00eb4200e1711a639bda05c
George Treviranus <3946669+geotrev@users.noreply.github.com>
2021-04-19 14:03:59 -0700

    Update create-store.js

--- app/src/store/create-store.js
+++ app/src/store/create-store.js
@@ -1,22 +1,9 @@
 import { store } from "../utils"
 import * as actionTypes from "./action-types"
-import * as actions from "./actions"
+import { reducer } from "./reducer"
 
 const initialState = {
   todos: [{ id: "foo-bar", draft: true }],
 }
 
-store.create(initialState, [
-  {
-    type: actionTypes.ADD_TODO,
-    dispatch: actions.addTodo,
-  },
-  {
-    type: actionTypes.SAVE_TODO,
-    dispatch: actions.saveTodo,
-  },
-  {
-    type: actionTypes.DELETE_TODO,
-    dispatch: actions.deleteTodo,
-  },
-])
+store.create(initialState, reducer)
