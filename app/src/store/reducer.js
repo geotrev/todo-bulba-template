15f710efb5de5530ba7d4ceb2f39e3045f69c105
George Treviranus <3946669+geotrev@users.noreply.github.com>
2021-04-19 14:00:49 -0700

    Update and rename actions.js to reducer.js

---
+++ app/src/store/reducer.js
@@ -0,0 +1,47 @@
+import { v4 as uid } from "uuid"
+
+export const reducer = (type, state, payload) => {
+  const {todos} = state
+  
+  switch (type) {
+    case 'ADD_TODO': {
+      const activeTodo = todos[0]
+      if (activeTodo && activeTodo.draft) return
+
+      todos.unshift({ id: uid(), value: "", draft: true })
+      return { todos }
+    }
+    case 'SAVE_TODO': {
+      const index = todos.findIndex((todo) => todo.id === payload.id)
+      
+      if (index === -1) {
+        // eslint-disable-next-line no-console
+        console.error(`[TODO]: Couldn't find todo with id ${payload.id}.`)
+        return state
+      }
+      
+      const activeTodo = todos[0]
+      
+      if (index === 0 && activeTodo.draft) delete activeTodo.draft
+
+      todos[index].value = payload.value
+      return { todos }
+    }
+    case 'DELETE_TODO': {
+      const index = todos.findIndex((item) => item.id === payload.id)
+
+      if (index === -1) {
+        // eslint-disable-next-line no-console
+        console.error(`[TODO]: Couldn't find todo with id ${payload.id}.`)
+        return state
+      }
+
+      todos.splice(index, 1)
+      return { todos }
+    }
+    default: {
+      return state
+    }
+  }
+}
+
