2de0916292b516376eaf6977e245449db2cddde5
George Treviranus <3946669+geotrev@users.noreply.github.com>
2021-04-19 14:48:48 -0700

    Update dispatch.js

--- app/src/utils/dispatch.js
+++ app/src/utils/dispatch.js
@@ -1,10 +1,10 @@
 import {REQUEST_STORE_UPDATE} from "./action-types"
 
-export const dispatch = (source, type, payload = {}) => {
+export const dispatch = (type, payload = {}) => {
   const event = new CustomEvent(REQUEST_STORE_UPDATE, {
     composed: true,
     bubbles: true,
     detail: { type, payload },
   })
-  source.dispatchEvent(event)
+  document.dispatchEvent(event)
 }
