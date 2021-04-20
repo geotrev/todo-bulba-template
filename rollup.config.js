6c5e738fbe1897f5c7f34b41646454f5ecfca425
George Treviranus <hello@geotrev.com>
2021-04-19 20:53:28 -0700

    add build script & update ignore files

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import path from "path"
+import { terser } from "rollup-plugin-terser"
 import { nodeResolve } from "@rollup/plugin-node-resolve"
 import serve from "rollup-plugin-serve"
 import livereload from "rollup-plugin-livereload"
@@ -7,10 +8,24 @@ import scss from "rollup-plugin-scss"
 import sass from "sass"
 import postcss from "postcss"
 import autoprefixer from "autoprefixer"
+import gzipPlugin from "rollup-plugin-gzip"
 
 const PUBLIC_PATH = path.resolve(__dirname, "public")
 const SOURCE_PATH = path.resolve(__dirname, "src/index.js")
 const OUTPUT_PATH = path.resolve(__dirname, "public/bundle.js")
+
+const terserPlugin = terser({
+  output: {
+    comments: function (node, comment) {
+      var text = comment.value
+      var type = comment.type
+      if (type == "comment2") {
+        // multiline comment
+        return /@preserve|@license|@cc_on/i.test(text)
+      }
+    },
+  },
+})
 
 const scssOptions = {
   processor: () => postcss([autoprefixer]),
@@ -18,23 +33,25 @@ const scssOptions = {
   output: false,
 }
 
+const plugins =
+  process.env.BUILD_ENV === "build"
+    ? [terserPlugin, gzipPlugin()]
+    : [
+        livereload({ watch: PUBLIC_PATH }),
+        serve({
+          open: true,
+          contentBase: PUBLIC_PATH,
+          historyApiFallback: true,
+          host: "localhost",
+          port: 3000,
+        }),
+      ]
+
 export default {
   input: SOURCE_PATH,
   output: {
     file: OUTPUT_PATH,
     format: "iife",
   },
-  plugins: [
-    nodeResolve(),
-    commonjs(),
-    livereload({ watch: PUBLIC_PATH }),
-    serve({
-      open: true,
-      contentBase: PUBLIC_PATH,
-      historyApiFallback: true,
-      host: "localhost",
-      port: 3000,
-    }),
-    scss(scssOptions),
-  ],
+  plugins: [nodeResolve(), commonjs(), scss(scssOptions), ...plugins],
 }
