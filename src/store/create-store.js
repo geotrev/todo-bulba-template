4275be788160716eff20731f88c5d65f2d37f342
George Treviranus <3946669+geotrev@users.noreply.github.com>
2021-04-20 10:55:58 -0700

    Update create-store.js

--- src/store/create-store.js
+++ src/store/create-store.js
@@ -1,8 +1,8 @@
-import { store } from "../utils"
+import { createStore } from "../utils"
 import { reducer } from "./reducer"
 
 const initialState = {
   todos: [{ id: "foo-bar", draft: true }],
 }
 
-store.create(initialState, reducer)
+createStore(initialState, reducer)
